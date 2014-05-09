a7e5518f6477f407c9827070abd63cd5903c0a51
nguyen.thanh.tungb <nguyen.thanh.tungb@framgia.com>
2014-05-09 10:04:09 +0700

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,12 @@ module SessionsHelper
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
   def sign_out
     current_user.update_attribute(:remember_token,
                                   User.digest(User.new_remember_token))
@@ -33,7 +39,6 @@ module SessionsHelper
     redirect_to(session[:return_to] || default)
     session.delete(:return_to)
   end
-
   def store_location
     session[:return_to] = request.url if request.get?
   end
